0de70920734970aa5ea55185169d30edd1725cb0
Kit Transue <src@kittransue.net>
2021-07-13 21:50:01 -0700

    SPM init +boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# TaskSemaphore
+
+A description of this package.
---
+++ Sources/TaskSemaphore/TaskSemaphore.swift
@@ -0,0 +1,15 @@
+//
+//  TaskSemaphore.swift
+//
+//
+//  Created by Kit Transue on 2021-07-13.
+//  Copyright © 2021 Kit Transue
+//  SPDX-License-Identifier: Apache-2.0
+//
+
+public struct TaskSemaphore {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
